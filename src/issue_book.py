218d1f3a7d729ff3dd8f3afb937a25ce07c1ffa7
parvi200514 <parviparaswani@gmail.com>
2026-06-21 18:51:38 +0530

    Upgrade issue_book.py

--- src/issue_book.py
+++ src/issue_book.py
@@ -1,3 +1,16 @@
+from datetime import date, timedelta
+
+
 class IssueBook:
+
     def issue(self, student_id, book_id):
-        return f"Book {book_id} issued to Student {student_id}"
+
+        transaction = {
+            "student_id": student_id,
+            "book_id": book_id,
+            "issue_date": str(date.today()),
+            "due_date": str(date.today() + timedelta(days=14)),
+            "status": "Issued"
+        }
+
+        return transaction
