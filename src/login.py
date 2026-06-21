28c6398dff19e7e279775f07bef555a9635058f7
parvi200514 <parviparaswani@gmail.com>
2026-06-21 18:51:14 +0530

    Upgrade login.py

--- src/login.py
+++ src/login.py
@@ -1,5 +1,14 @@
 class Login:
+
+    DEFAULT_USERNAME = "admin"
+    DEFAULT_PASSWORD = "admin123"
+
     def authenticate(self, username, password):
-        if username == "admin" and password == "admin123":
+
+        if (
+            username == self.DEFAULT_USERNAME
+            and password == self.DEFAULT_PASSWORD
+        ):
             return True
+
         return False
