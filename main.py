39f857f5ccdc659fc01717b4c3f679ddfa22ba0f
parvi200514 <parviparaswani@gmail.com>
2026-06-21 18:29:28 +0530

    Create application entry point

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Library Management System")
+root.geometry("800x500")
+
+title = Label(
+    root,
+    text="Library Management System",
+    font=("Arial", 20, "bold")
+)
+
+title.pack(pady=50)
+
+root.mainloop()
