d1244dbfbf0153ea3328797895523c9674e303a5
parvi200514 <parviparaswani@gmail.com>
2026-06-21 18:50:57 +0530

    Upgrade students.py

--- src/students.py
+++ src/students.py
@@ -1,10 +1,20 @@
 class Student:
-    def __init__(self, student_name, course, semester, phone, email):
-        self.student_name = student_name
+    def __init__(self, student_id, name,
+                 course, semester, phone, email):
+
+        self.student_id = student_id
+        self.name = name
         self.course = course
         self.semester = semester
         self.phone = phone
         self.email = email
 
-    def add_student(self):
-        return f"Student '{self.student_name}' added successfully."
+    def display_student(self):
+        return {
+            "Student ID": self.student_id,
+            "Name": self.name,
+            "Course": self.course,
+            "Semester": self.semester,
+            "Phone": self.phone,
+            "Email": self.email
+        }
